17525ad69e87ddea71606b4de931082d4efc38ae
Abulfazfa <abulfazfa@code.edu.az>
2024-07-16 17:48:27 +0400

    minOperation_H

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    d = 2
+    while n > 1:
+        while n % d == 0:
+            operations += d
+            n //= d
+        d += 1
+
+    return operations
+
+
+n = 9
+print(minOperations(n))
