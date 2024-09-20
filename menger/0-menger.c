2c3f5c951fdbc0012264c3f65f99ed4512594081
Abulfazfa <abulfazfa@code.edu.az>
2024-09-20 14:44:54 +0400

    task

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -10,24 +10,24 @@ int n, m, j, x, y;
 char s;
 
 m = pow(3, level);
-for (n = 0; n < m; n++)
+for (n = 0;n < m;n++)
 {
-    for (j = 0; j < m;)
-    {
-        s = '#';
-        x = n;
-        y = j++;
-        while (x > 0 || y > 0)
-        {
-            if (x % 3 == 1 && y % 3 == 1)
-            {
-                s = ' ';
-            }
-            x /= 3;
-            y /= 3;
-        }
-        printf("%c", s);
-    }
-    printf("\n");
+for (j = 0; j < m;)
+{
+s = '#';
+x = n;
+y = j++;
+while (x > 0 || y > 0)
+{
+if (x % 3 == 1 && y % 3 == 1)
+{
+s = ' ';
+}
+x /= 3;
+y /= 3;
+}
+printf("%c", s);
+}
+printf("\n");
 }
 }
