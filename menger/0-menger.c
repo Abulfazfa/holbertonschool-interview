bcb3fbf2e2956132f2bcaac5acd94d574241132b
Abulfazfa <abulfazfa@code.edu.az>
2024-09-20 14:43:26 +0400

    task

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -6,28 +6,28 @@
  */
 void menger(int level)
 {
-    int n, m, j, x, y;
-    char s;
+int n, m, j, x, y;
+char s;
 
-    m = pow(3, level);
-    for (n = 0; n < m; n++)
+m = pow(3, level);
+for (n = 0; n < m; n++)
+{
+    for (j = 0; j < m;)
     {
-        for (j = 0; j < m;)
+        s = '#';
+        x = n;
+        y = j++;
+        while (x > 0 || y > 0)
         {
-            s = '#';
-            x = n;
-            y = j++;
-            while (x > 0 || y > 0)
+            if (x % 3 == 1 && y % 3 == 1)
             {
-                if (x % 3 == 1 && y % 3 == 1)
-                {
-                    s = ' ';
-                }
-                x /= 3;
-                y /= 3;
+                s = ' ';
             }
-            printf("%c", s);
+            x /= 3;
+            y /= 3;
         }
-        printf("\n");
+        printf("%c", s);
     }
+    printf("\n");
+}
 }
