3b4efe80800771c6294b0931d7db4fcb7c036db3
Abulfazfa <abulfazfa@code.edu.az>
2024-10-30 10:02:36 +0400

    heap_sort

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,7 +1,11 @@
-#ifndef HEAP_SORT_H
-#define HEAP_SORT_H
-#include <stdlib.h>
+#ifndef _SORT_H_
+#define _SORT_H_
+
 #include <stdio.h>
-void heap_sort(int *array, size_t size);
+#include <stdlib.h>
+
 void print_array(const int *array, size_t size);
-#endif
+void swap(int *a, int *b, int *array, size_t n);
+void heap_root(int *arr, int n, int i, size_t size);
+void heap_sort(int *array, size_t size);
+#endif /* _SORT_H_ */
