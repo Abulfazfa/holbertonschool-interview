439c8e575cf356bb76b27974c8373236356136e7
Abulfazfa <abulfazfa@code.edu.az>
2024-10-30 10:01:52 +0400

    heap_sort

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,70 +1,75 @@
 #include "sort.h"
-/**
- * check_tree - swiftdown check
- * @array: pointer to array
- * @size: size of the pointer
- * @size_init: original size of the array
- * @i: index as a root of the tree
- *
- **/
-void check_tree(int *array, size_t size_init, size_t size, size_t i)
-{
 
-	int n, branch1, branch2;
-	size_t br1, br2;
-
-	br1 = i * 2 + 1;
-	br2 = br1 + 1;
-	branch1 = array[br1];
-	branch2 = array[br2];
-	if (((br1 < size) && (br2 < size) &&
-				(branch1 >= branch2 && branch1 > array[i]))
-			|| ((br1 == size - 1) && branch1 > array[i]))
-	{
-		n = array[i];
-		array[i] = branch1;
-		array[br1] = n;
-		print_array(array, size_init);
-	}
-	else if ((br1 < size) && (br2 < size) &&
-			(branch2 > branch1 && branch2 > array[i]))
-	{
-		n = array[i];
-		array[i] = branch2;
-		array[br2] = n;
-		print_array(array, size_init);
-	}
-	if (br1 < size - 1)
-		check_tree(array, size_init, size, br1);
-	if (br2 < size - 1)
-		check_tree(array, size_init, size, br2);
-}
 /**
- * heap_sort - sorts an array of integers
- * in ascending order using the Heap
- * sort algorithm
- * @array: pointer to array
- * @size: size of the pointer
- *
- **/
+ * heap_sort - Build max heap
+ * @array: array
+ * @size: size of array
+ */
 void heap_sort(int *array, size_t size)
 {
-	size_t i, size_init = size;
-	int n;
 
-	if (!array)
+	size_t i;
+
+	if (array == NULL)
 		return;
-	for (i = 0; i < size / 2 ; i++)
+
+	/* Build max heap */
+	for (i = size / 2 ; i > 0; i--)
+		heap_root(array, size, i - 1, size);
+
+	/* Heap sort */
+	for (i = size - 1; i > 0; i--)
 	{
-		check_tree(array, size_init, size, size / 2 - 1 - i);
+		swap(&array[0], &array[i], array, size);
+
+		/* Heapify root element to get highest element at root again */
+		heap_root(array, i, 0, size);
 	}
-	for (i = 0; i < size_init - 1; i++)
+}
+
+
+/**
+ * heap_root - Find largest among root, left child and right chil
+ * @arr: array
+ * @n: size array
+ * @i: current position
+ * @size: size
+ */
+void heap_root(int *arr, int n, int i, size_t size)
+{
+	/* Find largest among root, left child and right child */
+
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+
+	if (left < n && arr[left] > arr[largest])
+		largest = left;
+
+	if (right < n && arr[right] > arr[largest])
+		largest = right;
+
+	/* Swap and continue heapifying if root is not largest */
+	if (largest != i)
 	{
-		n = array[0];
-		array[0] = array[size - 1 - i];
-		array[size - 1 - i] = n;
-		print_array(array, size_init);
-		check_tree(array, size_init, size - i - 1, 0);
+		swap(&arr[i], &arr[largest], arr, size);
+		heap_root(arr, n, largest, size);
 	}
 
+}
+
+/**
+ * swap - Function to swap the the position of two elements
+ * @a: first integer
+ * @b: second integer
+ * @array: array of numbers
+ * @n: size of array
+ */
+void swap(int *a, int *b, int *array, size_t n)
+{
+
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+	print_array(array, n);
 }
